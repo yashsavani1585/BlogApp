e5c7699de56e917ee9a64abd485d01003c28e734
yashsavani <yashsavani1585@gmail.com>
2024-12-12 15:37:32 +0530

    five-commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure Vite outputs to `dist/`
+    outDir: 'dist/' // Ensure Vite outputs to `dist/`
   }
 })
