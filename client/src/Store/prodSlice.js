169ee33d15e336a8eceb15cb05446c0a3f561fbe
yash savani <yashsavani1585@gmail.com>
2024-12-12 17:04:39 +0530

    Update prodSlice.js

--- client/src/Store/prodSlice.js
+++ client/src/Store/prodSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const prodSlice = createSlice({
   name: 'prod',
   initialState: {
-    link: "https://blogapp-1-56pl.onrender.com"
+    link: "https://blogapp-back-4ooc.onrender.com"
   },
   reducers: {
     setLink: (state, action) => {
